150116d59ece20cf0ee33e95d8a726a34f70c58f
Rey Diaz <reybdiazjr@gmail.com>
2019-10-15 20:32:24 -0400

    set ups body parsers and routes to skeleton

---
+++ FriendFinder/server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyparser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 3000;
+
+// set up body parser returns json and string text and sets type
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true}));
+app.use(bodyparser.text());
+app.use(bodyparser.json({ type: "application/vnd.api+json"}));
+
+// routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
